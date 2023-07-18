a6472a7ca6b2e742db20a2356be6d2151c7da9f8
ALI-AJ-93 <testitmail93@gmail.com>
2023-07-18 17:47:35 +0300

    Add chapter about variables and datatypes

---
+++ variable_and_datatypes/bin/example1.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_local_variable
+
+void main(List<String> args) {
+  final bool someBoolean = !true;
+  print(someBoolean);
+}
+
+// datatype inference
+
+// dart bin/example1.dart
